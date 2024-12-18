1bf81d31ef3bb0956516e24fb7ca91638f518f65
arifayduran <arifayduran@users.noreply.github.com>
2024-12-18 20:53:09 +0000

    deploy: 1c9dcb476d25d3b2afe850e944417c5301b8d60e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4000086321"
+    serviceWorkerVersion: "1858426143"
   }
 });
